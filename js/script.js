09b7bc0a7eedb2d3b17d137a5bfe0e52e8546b70
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-26 17:46:30 +0200

    EDIT img string in img tag

--- js/script.js
+++ js/script.js
@@ -3,50 +3,57 @@ const members = [
         firstName: 'Wayne',
         lastName: 'Barnett',
         role: 'Founder & CEO',
-        photo: 'wayne-barnett-founder-ceo.jpg'
+        photo: '<img src="img/wayne-barnett-founder-ceo.jpg" alt="">'
     },
     {
         firstName: 'Angela',
         lastName: 'Caroll',
         role: 'Chief Editor',
-        photo: 'angela-caroll-chief-editor.jpg'
+        photo: '<img src="img/angela-caroll-chief-editor.jpg" alt="">'
     },
     {
         firstName: 'Walter',
         lastName: 'Gordon',
         role: 'Office Manager',
-        photo: 'walter-gordon-office-manager.jpg'
+        photo: '<img src="img/walter-gordon-office-manager.jpg" alt="">'
     },
     {
         firstName: 'Angela',
         lastName: 'Lopez',
         role: 'Social Media Manager',
-        photo: 'angela-lopez-social-media-manager.jpg'
+        photo: '<img src="img/angela-lopez-social-media-manager.jpg" alt="">'
     },
     {
         firstName: 'Scott',
         lastName: 'Estrada',
         role: 'Developer',
-        photo: 'scott-estrada-developer.jpg'
+        photo: '<img src="img/scott-estrada-developer.jpg" alt="">'
     },
     {
         firstName: 'Barbara',
         lastName: 'Ramos',
         role: 'Graphic Designer',
-        photo: 'barbara-ramos-graphic-designer.jpg'
+        photo: '<img src="img/barbara-ramos-graphic-designer.jpg" alt="">'
     }
 ]
 
 
 for (let index = 0; index < members.length; index++) {
     const newCard = document.createElement('div');
-
     const singleobject = members[index];
     console.log(singleobject)
-    for(const key in singleobject) {
-        console.log(key, singleobject[key]);
-        newCard.append(singleobject[key]);
-        document.querySelector('.container').append(newCard)
-        newCard.classList.add('card')
+    
+
+    newCard.innerHTML = (singleobject.photo)
+   // newCard.append(singleobject.firstName);
+    //newCard.append(singleobject.lastName);
+    document.querySelector('.container').append(newCard)
+    newCard.classList.add('card')
+    const images = document.querySelectorAll('img')
+
+    for (let index = 0; index < images.length; index++) {
+        const element = images[index];
+        element.classList.add('img-spec')   
     }
+    
 }
